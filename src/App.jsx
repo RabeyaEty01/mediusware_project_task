b4c41a1fc5befa829116c16cbac07da81935213c
rabeyaety01 <etyrabeya01@gmail.com>
2023-09-15 02:12:14 +0600

    query client added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { QueryClientProvider } from "react-query";
 import { Route, Routes } from "react-router-dom";
 import AllContacts from "./components/Contacts/AllContacts.jsx";
 import USContacts from "./components/Contacts/USContacts.jsx";
@@ -5,19 +6,20 @@ import Index from "./components/Index.jsx";
 import Menu from "./components/Menu.jsx";
 import Problem1 from "./components/Problem-1.jsx";
 import Problem2 from "./components/Problem-2.jsx";
-
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/" element={<Menu />}>
-          <Route path="problem-1" element={<Problem1 />} />
-          <Route path="problem-2" element={<Problem2 />} />
-        </Route>
-        <Route path="all-contacts" element={<AllContacts />} />
-        <Route path="us-contacts" element={<USContacts />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Menu />}>
+            <Route path="problem-1" element={<Problem1 />} />
+            <Route path="problem-2" element={<Problem2 />} />
+          </Route>
+          <Route path="all-contacts" element={<AllContacts />} />
+          <Route path="us-contacts" element={<USContacts />} />
+        </Routes>
+      </QueryClientProvider>
     </>
   );
 }
